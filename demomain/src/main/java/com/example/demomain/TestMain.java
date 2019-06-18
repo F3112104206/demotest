7715b4cc24940d10e8365a75632660209f2bba5c
feng667788 <2549900982@qq.com>
2019-06-18 17:30:30 +0800

    erere

--- demomain/src/main/java/com/example/demomain/TestMain.java
+++ demomain/src/main/java/com/example/demomain/TestMain.java
@@ -5,16 +5,13 @@ package com.example.demomain;
  * 实现对于项目的主干分支管上面实现对应数据提交
  */
 public class TestMain {
+
+
+    String name ="99999";
+
+
     public static void main(String[] args) {
-        System.out.printf("zhe ge shi dev fenzhi ");
         System.out.printf("master have this data");
-        System.out.printf("zhege di sange ");
-
     }
-
-
-    String name ="jjjjj";
-
-
 
 }
