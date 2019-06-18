1cadafc29abe623b44076665be6613360675c738
feng667788 <2549900982@qq.com>
2019-06-18 16:33:41 +0800

    实现提交实现

---
+++ demomain/src/main/java/com/example/demomain/DemomainApplication.java
@@ -0,0 +1,13 @@
+package com.example.demomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemomainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemomainApplication.class, args);
+    }
+
+}
